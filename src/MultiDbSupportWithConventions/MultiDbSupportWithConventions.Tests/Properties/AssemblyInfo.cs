2d8cb690ad766fcd06392dcd160a74be4de384d3
Jonathan Channon <jonathan.channon@gmail.com>
2015-12-15 18:03:26 +0000

    added assemblyinfo attribute to diabled parallel tests due to bug in FIE

--- src/MultiDbSupportWithConventions/MultiDbSupportWithConventions.Tests/Properties/AssemblyInfo.cs
+++ src/MultiDbSupportWithConventions/MultiDbSupportWithConventions.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+using Xunit;
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -37,3 +39,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)] //TODO Remove when FakeItEasy releases version > 1.25.3
